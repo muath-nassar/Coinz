fd040b13acde3f10c97e3b4e9660add9fb4bb3d9
Muath Nassar <moadnassar@gmail.com>
2020-07-23 11:27:49 -0700

    Splash screen is done

---
+++ app/src/main/java/com/example/coinz/SplashScreen.kt
@@ -0,0 +1,17 @@
+package com.example.coinz
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+
+class SplashScreen : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash_screen)
+        // Hide the status bar.
+        window.decorView.systemUiVisibility = View.SYSTEM_UI_FLAG_FULLSCREEN
+// Remember that you should never show the action bar if the
+// status bar is hidden, so hide that too if necessary.
+        actionBar?.hide()
+    }
+}
